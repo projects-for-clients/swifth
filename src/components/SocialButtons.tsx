147ea4fb688cee19c2a97c5a52863a58426a81b2
Igboanugwo Collins <munisco12@gmail.com>
2022-12-11 09:57:04 +0100

    working on social buttons

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -6,10 +6,24 @@ interface ISocialButtons {
 
 const SocialButtons: React.FC<{}> = () => {
   return (
-    <div>
-      <button>Continue with Email</button>
-      <button>Continue with Google</button>
-      <button>Continue with Facebook</button>
+    <div className="socialButtons">
+      <h1>Join Swifth</h1>
+      <div className="socialButtons__container">
+        <section className="socialButtons__box">
+          <button>Continue with Email</button>
+          <button>Continue with Google</button>
+          <button>Continue with Facebook</button>
+        </section>
+
+        <p>
+          I have an account, <button> Log In</button>
+        </p>
+      </div>
+      <p>
+        By signing you confirm to have read Swifth's{' '}
+        <a href="/"> Privacy policy</a> and agree to the{' '}
+        <a href="/">Terms of service</a>
+      </p>
     </div>
   );
 };
