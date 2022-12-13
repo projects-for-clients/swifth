f632bda46d6c7a121bb83774686e4e86f6ec792c
Igboanugwo Collins <munisco12@gmail.com>
2022-12-13 12:18:40 +0100

    working on auth

--- src/components/AuthSteps/FourthStep.tsx
+++ src/components/AuthSteps/FourthStep.tsx
@@ -6,7 +6,7 @@ import { selectUser } from '../../store/features/user/user';
 function ThirdAuthStep() {
 
   const [password, setPassword] = useState('')
-
+  const [loading, setLoading] = useState(false)
  
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -17,31 +17,31 @@ function ThirdAuthStep() {
 
   return (
     <div className="grid gap-10 mt-16 justify-center pb-5">
-      <h2 className="heading2 text-center">We just sent your an email</h2>
+      <h2 className="heading2 text-center">Create your password</h2>
 
       <form className="grid w-full form" onSubmit={handleSubmit}>
         {' '}
         <div className="form__input">
-          <label className="input__label">First Name</label>
+          <label className="input__label">Create Password</label>
           <input
             type="text"
-            placeholder="Enter first name"
+            placeholder="create a password..."
             className="input__item"
-            name="firstName"
-            defaultValue={inputField.firstName}
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
+          <p className='input__label'>Choose a password you will always remember</p>
         </div>
         <button
           className="bg-[#40AD6B] text-[1.6rem] py-6 disabled:opacity-50 btn1"
           type="submit"
-          disabled={isOtpLengthInValid}
+          disabled={password.length < 6}
         >
           {loading ? 'Loading...' : 'Continue'}
         </button>
       </form>
-      <p className="authText mt-10">
-        Didn't get a code? <button> Resend code</button>
-      </p>
+     
     </div>
   );
 }
