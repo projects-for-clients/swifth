b356c4de41445dd7869016c021a92bc5e9d3a7f7
Igboanugwo Collins <munisco12@gmail.com>
2022-12-29 12:30:05 +0100

    working on dashboard sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -3,10 +3,12 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__container">
-        <section className='section-1'>
-            <img src="/logo-white.svg" alt="" width={28} height={28} />
-          <h3 className="header__left--logoText">Swifth</h3>
+        <section className="section-1">
           <BiMenu />
+          <div>
+            <img src="/logo-white.svg" alt="" width={28} height={28} />
+          </div>
+          <h3>Swifth</h3>
         </section>
         <section>second</section>
         <section>third</section>
