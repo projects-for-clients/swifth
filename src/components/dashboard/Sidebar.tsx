fe54b521d04e853b507637d2c546839f8981b279
Igboanugwo Collins <munisco12@gmail.com>
2022-12-29 12:44:43 +0100

    working on dashboard sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,16 +1,20 @@
 import { BiMenu } from 'react-icons/bi';
+
+
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__container">
         <section className="section-1">
           <BiMenu />
-          <div className='section-1__box'>
+          <div className="section-1__box">
             <img src="/logo-white.svg" alt="" width={28} height={28} />
             <h3>Swifth</h3>
           </div>
         </section>
-        <section>second</section>
+        <section>
+          <img src="/dashboard/home.svg" alt="" width={24} height={24} />
+        </section>
         <section>third</section>
       </div>
     </div>
