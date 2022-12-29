258c32d2a846d0d94d8fc789452747b55b2ef86b
Igboanugwo Collins <munisco12@gmail.com>
2022-12-29 11:40:01 +0100

    working on dashboard sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -2,11 +2,13 @@ import { BiMenu } from 'react-icons/bi';
 function Sidebar() {
   return (
     <div className="sidebar">
-      <section>
-        <BiMenu />
-      </section>
-      <section>second</section>
-      <section>third</section>
+      <div className="sidebar__container">
+        <section>
+          <BiMenu />
+        </section>
+        <section>second</section>
+        <section>third</section>
+      </div>
     </div>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,8 +5,23 @@ function Dashboard() {
   return (
     <div className='dashboard'>
 
+
     <Sidebar/>
-    <div>Dashboard</div>
+    <div className=''>Dashboard
+
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam accusantium sequi unde maxime possimus dolor corporis quisquam fuga quo facilis cumque tenetur, ipsum et quaerat voluptatibus. Enim nihil aliquam reprehenderit.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae pariatur unde, alias recusandae laboriosam nam nesciunt aperiam totam assumenda, ratione deleniti aliquid cumque animi aspernatur saepe, ullam deserunt obcaecati? Quaerat.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, obcaecati earum illo impedit aspernatur quasi et placeat eaque. Saepe tenetur, temporibus esse sapiente neque accusamus omnis ut doloribus quam ratione!
+        Mollitia corrupti, doloremque expedita ea iusto laborum exercitationem repudiandae nihil maxime, corporis, ut debitis neque non ab dolores magnam commodi. Maiores esse necessitatibus veritatis, placeat quam id eos consectetur voluptatem?
+        Quos, quisquam! At tempora ex quae dolorum accusamus id alias fugit officiis aliquid in aliquam eum nam distinctio totam ratione voluptate beatae, deleniti natus culpa eveniet iure, libero expedita ullam!
+        Quos soluta, aperiam, vero eius deleniti aliquid, sunt harum ducimus debitis aut maiores explicabo officiis nisi accusantium rerum tempore doloremque voluptatem molestiae cum. Corporis molestias voluptas provident, officiis debitis temporibus.
+        Amet laborum placeat nostrum ea quasi necessitatibus vero dolores reprehenderit magni! Consequuntur reprehenderit odio laboriosam soluta eaque necessitatibus odit quibusdam vitae est quo adipisci minima voluptatibus, possimus fugit provident. Reprehenderit.
+        Sunt possimus, consectetur illo fuga tenetur recusandae velit iure est, nulla alias aperiam amet quisquam maiores quas ut ex enim molestiae harum aliquid dolores doloribus perferendis exercitationem dolore. Aliquid, exercitationem!
+        Laboriosam possimus, ipsum nam ut maxime ullam quaerat! Mollitia rerum, corrupti, et totam vel nobis recusandae porro minima eaque quod adipisci illum fugiat repellat dolorum sit, laboriosam excepturi saepe accusantium?
+        Pariatur veniam officiis vel maiores! Illo ut voluptates obcaecati temporibus cum nam corrupti quos esse veniam rerum, nihil laborum asperiores eaque dolor rem officiis nostrum dolorum dolore? A, nam tempore?
+        Dolorum impedit aperiam qui tenetur architecto voluptate? Sit delectus laborum aliquid amet quisquam reprehenderit iure error, incidunt iste excepturi. Et, libero. Consequatur praesentium fuga odit sequi voluptas qui assumenda illo?
+        Consequatur, ab ea dolor perspiciatis, saepe non suscipit reiciendis cum magni consequuntur incidunt ipsa dolores possimus temporibus, unde ipsam? Nam eum voluptas optio consequuntur exercitationem at ipsa unde omnis harum!
+    </div>
     </div>
   )
 }
