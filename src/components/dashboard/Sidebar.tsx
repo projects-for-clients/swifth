c830f75a7364604f9209215ae2d44793a43ab42a
Igboanugwo Collins <munisco12@gmail.com>
2022-12-29 10:27:03 +0100

    working on dashboard sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,13 +1,14 @@
-
+import { BiMenu } from 'react-icons/bi';
 function Sidebar() {
   return (
-    <div className='sidebar'>
-        <section>first</section>
-        <section>second</section>
-        <section>third</section>
-
+    <div className="sidebar">
+      <section>
+        <BiMenu />
+      </section>
+      <section>second</section>
+      <section>third</section>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
