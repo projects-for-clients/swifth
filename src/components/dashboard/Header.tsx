e5a51452f9d60729d5125bec0e77882e83fd13c5
Igboanugwo Collins <munisco12@gmail.com>
2023-01-01 20:18:40 +0100

    working on sidebar

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function Header() {
-  return (
-    <div className='header'>
-<div className="header__left"></div>
-<div className="header__right"></div>
-    </div>
-  )
+type THeader = {
+    title: string
+    subTitle: string
 }
 
-export default Header
+function Header({title, subTitle}: THeader) {
+  return (
+    <div className="header">
+      <div className="header__left">
+
+      </div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,7 +9,7 @@ function Dashboard() {
 
     <Sidebar/>
     <div className='dashboard__container'>
-      <Header/>
+      <Header title='Hello, Nachi' subTitle='Welcome to Swifth'/>
 
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam accusantium sequi unde maxime possimus dolor corporis quisquam fuga quo facilis cumque tenetur, ipsum et quaerat voluptatibus. Enim nihil aliquam reprehenderit.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae pariatur unde, alias recusandae laboriosam nam nesciunt aperiam totam assumenda, ratione deleniti aliquid cumque animi aspernatur saepe, ullam deserunt obcaecati? Quaerat.
