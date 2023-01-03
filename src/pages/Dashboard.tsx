4f67482c1e935c2b96dff12cf183e36843bbf269
Igboanugwo Collins <munisco12@gmail.com>
2023-01-03 12:32:18 +0100

    working on header

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from '../components/dashboard/Header';
 import Sidebar from '../components/dashboard/Sidebar';
 
@@ -8,7 +7,17 @@ function Dashboard() {
       <Sidebar />
       <div className="dashboard__container">
         <Header title="Hello, Nachi" subTitle="Welcome to Swifth" />
+
+        <div>
+          <h3>Account Setup</h3>
+          <p>
+            It’s time to set up your account. We will need a few things to get
+            you going
+          </p>
+          <div>
             
+          </div>
+        </div>
       </div>
     </div>
   );
