899146d23745efcdeb569ddf43e82db26a8c24ef
Igboanugwo Collins <munisco12@gmail.com>
2023-01-03 18:43:47 +0100

    working on sidebar

--- src/container/dashboard/home.tsx
+++ src/container/dashboard/home.tsx
@@ -52,42 +52,9 @@ function home() {
             </div>
           </div>
           <div className="home__left-item">
-            <h2 className="heading3">Account Setup</h2>
-            <p className="text">
-              It's time to set up your account. We will need a few things to get
-              you going
-            </p>
-            <div className="left-item__box">
-              <div className="left-item__progress">
-                <p>0%</p> <p>Completed</p>
-              </div>
-              <button className="left-item__btn">Get Started</button>
-            </div>
-            <div className="left-item__steps">
-              <div className="left-item__step">
-                <EllipseSvg />
-                <div className="left-item__step--label">
-                  <p className="text">Your business information</p>
-                  <FiChevronDown />
-                </div>
-              </div>
-              <div className="left-item__step">
-                <CheckmarkSvg />
-                <div className="left-item__step--label">
-                  {' '}
-                  <p className="text">Port and terminal Info</p>
-                  <FiChevronDown />
-                </div>
-              </div>
-              <div className="left-item__step">
-                <CancelSvg />
-                <div className="left-item__step--label">
-                  {' '}
-                  <p className="text">Contact information</p>
-                  <FiChevronDown />
-                </div>
-              </div>
-            </div>
+            <h2 className="heading3">Business analysis</h2>
+           
+          
           </div>
         </section>
         <section className="home__right">
