a2bc49cdf3ec25682c4d100c577a10cd1f7b2d3a
Igboanugwo Collins <munisco12@gmail.com>
2023-01-03 14:11:55 +0100

    working on dashboard content

--- src/container/dashboard/home.tsx
+++ src/container/dashboard/home.tsx
@@ -5,30 +5,32 @@ function home() {
     <>
       <Header title="Hello, Nachi" subTitle="Welcome to Swifth" />
 
-      <div className='dashboard__home'>
-        <h3>Account Setup</h3>
-        <p>
-          It’s time to set up your account. We will need a few things to get you
-          going
-        </p>
-        <div>
-          <div>box 1</div>
-          <div>box 2</div>
-        </div>
-        <div>
+      <div className="dashboard__home">
+        <section>
+          <h3 className="">Account Setup</h3>
+          <p>
+            It’s time to set up your account. We will need a few things to get
+            you going
+          </p>
           <div>
-            <input type="radio" name="businessInfo" id="businessInfo" />
-            <label htmlFor="businessInfo">Your business information</label>
+            <div>box 1</div>
+            <div>box 2</div>
           </div>
           <div>
-            <input type="radio" name="portAndTerminal" id="portAndTerminal" />
-            <label htmlFor="portAndTerminal">Port and terminal Info</label>
+            <div>
+              <input type="radio" name="businessInfo" id="businessInfo" />
+              <label htmlFor="businessInfo">Your business information</label>
+            </div>
+            <div>
+              <input type="radio" name="portAndTerminal" id="portAndTerminal" />
+              <label htmlFor="portAndTerminal">Port and terminal Info</label>
+            </div>
+            <div>
+              <input type="radio" name="contactInfo" id="contactInfo" />
+              <label htmlFor="contactInfo">Contact information</label>
+            </div>
           </div>
-          <div>
-            <input type="radio" name="contactInfo" id="contactInfo" />
-            <label htmlFor="contactInfo">Contact information</label>
-          </div>
-        </div>
+        </section>
       </div>
     </>
   );
