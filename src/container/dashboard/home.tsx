13ac294f726883474ed870c2b743367ebd7ad0af
Igboanugwo Collins <munisco12@gmail.com>
2023-01-03 18:39:43 +0100

    working on sidebar

--- src/container/dashboard/home.tsx
+++ src/container/dashboard/home.tsx
@@ -13,37 +13,37 @@ function home() {
 
       <div className="dashboard__home">
         <section className="home__left">
-          <div className="home__left-1">
+          <div className="home__left-item">
             <h2 className="heading3">Account Setup</h2>
             <p className="text">
               It's time to set up your account. We will need a few things to get
               you going
             </p>
-            <div className="left-1__box">
-              <div className="left-1__progress">
+            <div className="left-item__box">
+              <div className="left-item__progress">
                 <p>0%</p> <p>Completed</p>
               </div>
-              <button className="left-1__btn">Get Started</button>
+              <button className="left-item__btn">Get Started</button>
             </div>
-            <div className="left-1__steps">
-              <div className="left-1__step">
+            <div className="left-item__steps">
+              <div className="left-item__step">
                 <EllipseSvg />
-                <div className="left-1__step--label">
+                <div className="left-item__step--label">
                   <p className="text">Your business information</p>
                   <FiChevronDown />
                 </div>
               </div>
-              <div className="left-1__step">
+              <div className="left-item__step">
                 <CheckmarkSvg />
-                <div className="left-1__step--label">
+                <div className="left-item__step--label">
                   {' '}
                   <p className="text">Port and terminal Info</p>
                   <FiChevronDown />
                 </div>
               </div>
-              <div className="left-1__step">
+              <div className="left-item__step">
                 <CancelSvg />
-                <div className="left-1__step--label">
+                <div className="left-item__step--label">
                   {' '}
                   <p className="text">Contact information</p>
                   <FiChevronDown />
@@ -53,14 +53,27 @@ function home() {
           </div>
         </section>
         <section className="home__right">
-          <div className="home__right-1">
+          <div className="home__right-item">
             <FinanceSvg fill={'#957979'} />
-            <div className="right-1__content">
+            <div className="right-item__content">
               <h2 className="heading3">Total Earnings</h2>
               <p className="heading2">NGN 0.0</p>
             </div>
           </div>
-          <div>left two</div>
+          <div className="home__right-item">
+            <FinanceSvg fill={'#957979'} />
+            <div className="right-item__content">
+              <h2 className="heading3">Total Earnings</h2>
+              <p className="heading2">NGN 0.0</p>
+            </div>
+          </div>
+          <div className="home__right-item">
+            <FinanceSvg fill={'#957979'} />
+            <div className="right-item__content">
+              <h2 className="heading3">Total Earnings</h2>
+              <p className="heading2">NGN 0.0</p>
+            </div>
+          </div>
         </section>
       </div>
     </>
