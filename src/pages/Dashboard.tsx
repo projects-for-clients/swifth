0aa3239fa7e7861fef84538a1df63666c7310255
Igboanugwo Collins <munisco12@gmail.com>
2023-01-03 12:22:31 +0100

    working on header

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,37 +8,7 @@ function Dashboard() {
       <Sidebar />
       <div className="dashboard__container">
         <Header title="Hello, Nachi" subTitle="Welcome to Swifth" />
-        litia
-        corrupti, doloremque expedita ea iusto laborum exercitationem
-        repudiandae nihil maxime, corporis, ut debitis neque non ab dolores
-        magnam commodi. Maiores esse necessitatibus veritatis, placeat quam id
-        eos consectetur voluptatem? Quos, quisquam! At tempora ex quae dolorum
-        accusamus id alias fugit officiis aliquid in aliquam eum nam distinctio
-        totam ratione voluptate beatae, deleniti natus culpa eveniet iure,
-        libero expedita ullam! Quos soluta, aperiam, vero eius deleniti aliquid,
-        sunt harum ducimus debitis aut maiores explicabo officiis nisi
-        accusantium rerum tempore doloremque voluptatem molestiae cum. Corporis
-        molestias voluptas provident, officiis debitis temporibus. Amet laborum
-        placeat nostrum ea quasi necessitatibus vero dolores reprehenderit
-        magni! Consequuntur reprehenderit odio laboriosam soluta eaque
-        necessitatibus odit quibusdam vitae est quo adipisci minima
-        voluptatibus, possimus fugit provident. Reprehenderit. Sunt possimus,
-        consectetur illo fuga tenetur recusandae velit iure est, nulla alias
-        aperiam amet quisquam maiores quas ut ex enim molestiae harum aliquid
-        dolores doloribus perferendis exercitationem dolore. Aliquid,
-        exercitationem! Laboriosam possimus, ipsum nam ut maxime ullam quaerat!
-        Mollitia rerum, corrupti, et totam vel nobis recusandae porro minima
-        eaque quod adipisci illum fugiat repellat dolorum sit, laboriosam
-        excepturi saepe accusantium? Pariatur veniam officiis vel maiores! Illo
-        ut voluptates obcaecati temporibus cum nam corrupti quos esse veniam
-        rerum, nihil laborum asperiores eaque dolor rem officiis nostrum dolorum
-        dolore? A, nam tempore? Dolorum impedit aperiam qui tenetur architecto
-        voluptate? Sit delectus laborum aliquid amet quisquam reprehenderit iure
-        error, incidunt iste excepturi. Et, libero. Consequatur praesentium fuga
-        odit sequi voluptas qui assumenda illo? Consequatur, ab ea dolor
-        perspiciatis, saepe non suscipit reiciendis cum magni consequuntur
-        incidunt ipsa dolores possimus temporibus, unde ipsam? Nam eum voluptas
-        optio consequuntur exercitationem at ipsa unde omnis harum!
+            
       </div>
     </div>
   );
