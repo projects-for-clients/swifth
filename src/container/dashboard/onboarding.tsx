3ebcc2f6879fe123d0d24c8539ade2e9b6e6cb34
Igboanugwo Collins <munisco12@gmail.com>
2023-01-21 16:18:27 +0100

    working on onboarding

--- src/container/dashboard/onboarding.tsx
+++ src/container/dashboard/onboarding.tsx
@@ -1,6 +1,54 @@
+import { MouseEvent, useEffect, useReducer, useState } from 'react';
 import Header from '../../components/dashboard/Header';
+import { getPhotoUrl } from '../../utils/getPhotoUrl';
 
 const onboarding = () => {
+  interface PhotoUrl {
+    cacUploadUrl?: string;
+    licenseUploadUrl?: string;
+  }
+
+  const [photoUrl, setPhotoUrl] = useReducer(
+    (prev: PhotoUrl, next: PhotoUrl): PhotoUrl => {
+      return {
+        ...prev,
+        ...next,
+      };
+    },
+    {
+      cacUploadUrl: '',
+      licenseUploadUrl: '',
+    }
+  );
+
+  
+  const uploadPhotoHandler = async (
+    file: MouseEvent<HTMLInputElement>,
+    value: string,
+    type: 'cac' | 'license'
+  ) => {
+
+    console.log('e', e.target.files);
+    let getCacUploadUrl = '';
+    let getLicenseUploadUrl = '';
+
+    if (type === 'cac') {
+      getCacUploadUrl = await getPhotoUrl(value);
+    } else {
+      getLicenseUploadUrl = await getPhotoUrl(value);
+    }
+
+    console.log(getCacUploadUrl, getLicenseUploadUrl);
+
+    setPhotoUrl({
+      cacUploadUrl: getCacUploadUrl ?? '',
+      licenseUploadUrl: getLicenseUploadUrl ?? '',
+    });
+  };
+
+  useEffect(() => {
+    console.log({photoUrl});
+  }, [photoUrl.cacUploadUrl, photoUrl.licenseUploadUrl]);
   return (
     <>
       <Header
@@ -26,14 +74,36 @@ const onboarding = () => {
           </figure>
 
           <div className="grid grid-cols-2 gap-4 items-center justify-between">
-            <div className="flex border border-color-purple-light rounded-lg py-8 px-10 items-center gap-6 cursor-pointer">
+            <label
+              htmlFor="cacUpload"
+              className="flex border border-color-purple-light rounded-lg py-8 px-10 items-center gap-6 cursor-pointer"
+            >
               <img src="/icons/admin/upload.svg" alt="" />
               <p>Upload CAC Certificate</p>
-            </div>
-            <div className="flex border border-color-purple-light rounded-lg py-8 px-10 items-center gap-6 cursor-pointer">
+            </label>
+            <input
+              type="file"
+              name="cacUpload"
+              id="cacUpload"
+              accept="image/*"
+              // className="hidden"
+              onClick={(e) => uploadPhotoHandler(e, 'cacUpload', 'cac')}
+            />
+            <label
+              htmlFor="licenseUpload"
+              className="flex border border-color-purple-light rounded-lg py-8 px-10 items-center gap-6 cursor-pointer"
+            >
               <img src="/icons/admin/upload.svg" alt="" />
               <p>Upload Custom License (yearly)</p>
-            </div>
+            </label>
+            <input
+              type="file"
+              name="licenseUpload"
+              id="licenseUpload"
+              accept="image/*"
+              className="hidden"
+              onClick={(e) => uploadPhotoHandler(e, 'licenseUpload', 'license')}
+            />
           </div>
         </div>
       </div>
---
+++ src/utils/getPhotoUrl.ts
@@ -0,0 +1,26 @@
+export const getPhotoUrl = async(selector: string) => {
+    const selectPhoto = document.querySelector(`#${selector}`)
+
+    if (!selectPhoto) return 'no photo selected'
+
+    const photo = selectPhoto as HTMLInputElement
+
+
+    const photoUrl:string = await new Promise((resolve, reject) => {
+        photo.addEventListener('change', (e) => {
+            const file = e.target as HTMLInputElement
+
+            
+            const reader = new FileReader()
+            console.log({reader})
+
+            reader.readAsDataURL(file.files![0])
+
+            reader.onload = () => resolve(reader.result as string)
+
+            reader.onerror = (err) => reject(err)
+        })
+    })
+
+    return photoUrl
+}
