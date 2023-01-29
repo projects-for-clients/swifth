64aa5033bf49d017e03d8e2697ea4f16305ff9e2
Igboanugwo Collins <munisco12@gmail.com>
2023-01-29 08:30:20 +0100

    working on personal info

--- src/components/OnboardingSteps/PersonalInfo.tsx
+++ src/components/OnboardingSteps/PersonalInfo.tsx
@@ -43,7 +43,7 @@ const PersonalInfo = () => {
 
   const uploadUriHandler = async (
     e: MouseEvent<HTMLInputElement>,
-    key: 'idCardUri' | 'pOAUri' | 'logoUri'
+    key: 'idCardUri' | 'POAUri'
   ) => {
     const getUri = await getPhotoUri(key);
 
@@ -54,12 +54,12 @@ const PersonalInfo = () => {
       },
     } as ChangeEvent<HTMLInputElement>;
 
-    handleInputChange(data, 'businessInfo');
+    handleInputChange(data, 'personalInfo');
   };
 
   const uploadDetailsHandler = (
     e: ChangeEvent<HTMLInputElement>,
-    type: 'idCard' | 'pOA' | 'logo'
+    type: 'idCard' | 'POA'
   ) => {
     const { files } = e.target;
 
@@ -95,7 +95,7 @@ const PersonalInfo = () => {
       if (type === 'idCard') {
         setIdCardDetails(path.name);
       }
-      if (type === 'pOA') {
+      if (type === 'POA') {
         setPOADetails(path.name);
       }
     }
@@ -151,106 +151,108 @@ const PersonalInfo = () => {
               />
             </div>
             <div className="grid grid-cols-2 gap-4 items-center justify-between col-span-full">
-            <label
-              htmlFor="idCardUri"
-              className={`flex border rounded-lg py-8 px-10 items-center gap-6 cursor-pointer h-[7rem] ${
-                (validationErrors && validationErrors.idCardUri) ||
-                imageSize.error.idCard
-                  ? 'border-red-600 border bg-red-50'
-                  : 'border-color-purple-light'
-              }`}
-            >
-              {(validationErrors && validationErrors.idCardUri) ||
-              imageSize.error.idCard ? (
-                <img src="/icons/admin/uploadError.svg" alt="" />
-              ) : (
-                <img src="/icons/admin/upload.svg" alt="" />
-              )}
+              <label
+                htmlFor="idCardUri"
+                className={`flex border rounded-lg py-8 px-10 items-center gap-6 cursor-pointer h-[7rem] ${
+                  (validationErrors && validationErrors.idCardUri) ||
+                  imageSize.error.idCard
+                    ? 'border-red-600 border bg-red-50'
+                    : 'border-color-purple-light'
+                }`}
+              >
+                {(validationErrors && validationErrors.idCardUri) ||
+                imageSize.error.idCard ? (
+                  <img src="/icons/admin/uploadError.svg" alt="" />
+                ) : (
+                  <img src="/icons/admin/upload.svg" alt="" />
+                )}
 
-              {idCardDetails ? (
-                <div className="grid">
-                  <p className="text-[1.4rem] font-normal">{idCardDetails}</p>
+                {idCardDetails ? (
+                  <div className="grid">
+                    <p className="text-[1.4rem] font-normal">{idCardDetails}</p>
 
-                  {imageSize?.error.idCard ? (
-                    <p className="text-red-600 text-[1.2rem]">
-                      File size must not exceed 2MB
+                    {imageSize?.error.idCard ? (
+                      <p className="text-red-600 text-[1.2rem]">
+                        File size must not exceed 2MB
+                      </p>
+                    ) : (
+                      <p className="text-color-grey-4 text-[1rem]">
+                        {imageSize.idCard}
+                      </p>
+                    )}
+                  </div>
+                ) : (
+                  <div className="grid">
+                    <p className="text-color-grey-3">
+                      Upload Contact Person ID Card
                     </p>
-                  ) : (
-                    <p className="text-color-grey-4 text-[1rem]">
-                      {imageSize.idCard}
-                    </p>
-                  )}
-                </div>
-              ) : (
-                <div className="grid">
-                  <p className="text-color-grey-3">Upload Contact Person ID Card</p>
 
-                  {validationErrors && validationErrors.idCardUri && (
-                    <p className="text-red-600 text-[1.2rem]">
-                      {validationErrors.idCardUri}
-                    </p>
-                  )}
-                </div>
-              )}
-            </label>
-            <input
-              type="file"
-              name="idCardUri"
-              id="idCardUri"
-              accept="image/*"
-              className="hidden"
-              onClick={(e) => uploadUriHandler(e, 'idCardUri')}
-              onChange={(e) => uploadDetailsHandler(e, 'idCard')}
-            />
-            <label
-              htmlFor="POAUri"
-              className={`flex border rounded-lg py-8 px-10 items-center gap-6 cursor-pointer h-[7rem] ${
-                (validationErrors && validationErrors.POAUri) ||
-                imageSize.error.POA
-                  ? 'border-red-600 border bg-red-50'
-                  : 'border-color-purple-light'
-              }`}
-            >
-              {(validationErrors && validationErrors.pOAUri) ||
-              imageSize.error.POA ? (
-                <img src="/icons/admin/uploadError.svg" alt="" />
-              ) : (
-                <img src="/icons/admin/upload.svg" alt="" />
-              )}
-              {POADetails ? (
-                <div className="grid">
-                  <p className="text-[1.4rem] font-normal">{POADetails}</p>
-                  {imageSize?.error.POA ? (
-                    <p className="text-red-600 text-[1.2rem]">
-                      File size must not exceed 2MB
-                    </p>
-                  ) : (
-                    <p className="text-color-grey-4 text-[1rem]">
-                      {imageSize.POA}
-                    </p>
-                  )}
-                </div>
-              ) : (
-                <div className="grid">
-                  <p>Upload Custom POA (yearly)</p>
-                  {validationErrors && validationErrors.pOAUri && (
-                    <p className="text-red-600 text-[1.2rem]">
-                      {validationErrors.pOAUri}
-                    </p>
-                  )}
-                </div>
-              )}
-            </label>
-            <input
-              type="file"
-              name="pOAUri"
-              id="pOAUri"
-              accept="image/*"
-              className="hidden"
-              onClick={(e) => uploadUriHandler(e, 'pOAUri')}
-              onChange={(e) => uploadDetailsHandler(e, 'pOA')}
-            />
-          </div>
+                    {validationErrors && validationErrors.idCardUri && (
+                      <p className="text-red-600 text-[1.2rem]">
+                        {validationErrors.idCardUri}
+                      </p>
+                    )}
+                  </div>
+                )}
+              </label>
+              <input
+                type="file"
+                name="idCardUri"
+                id="idCardUri"
+                accept="image/*"
+                className="hidden"
+                onClick={(e) => uploadUriHandler(e, 'idCardUri')}
+                onChange={(e) => uploadDetailsHandler(e, 'idCard')}
+              />
+              <label
+                htmlFor="POAUri"
+                className={`flex border rounded-lg py-8 px-10 items-center gap-6 cursor-pointer h-[7rem] ${
+                  (validationErrors && validationErrors.POAUri) ||
+                  imageSize.error.POA
+                    ? 'border-red-600 border bg-red-50'
+                    : 'border-color-purple-light'
+                }`}
+              >
+                {(validationErrors && validationErrors.pOAUri) ||
+                imageSize.error.POA ? (
+                  <img src="/icons/admin/uploadError.svg" alt="" />
+                ) : (
+                  <img src="/icons/admin/upload.svg" alt="" />
+                )}
+                {POADetails ? (
+                  <div className="grid">
+                    <p className="text-[1.4rem] font-normal">{POADetails}</p>
+                    {imageSize?.error.POA ? (
+                      <p className="text-red-600 text-[1.2rem]">
+                        File size must not exceed 2MB
+                      </p>
+                    ) : (
+                      <p className="text-color-grey-4 text-[1rem]">
+                        {imageSize.POA}
+                      </p>
+                    )}
+                  </div>
+                ) : (
+                  <div className="grid">
+                    <p>Upload Custom POA (yearly)</p>
+                    {validationErrors && validationErrors.pOAUri && (
+                      <p className="text-red-600 text-[1.2rem]">
+                        {validationErrors.pOAUri}
+                      </p>
+                    )}
+                  </div>
+                )}
+              </label>
+              <input
+                type="file"
+                name="POAUri"
+                id="POAUri"
+                accept="image/*"
+                className="hidden"
+                onClick={(e) => uploadUriHandler(e, 'POAUri')}
+                onChange={(e) => uploadDetailsHandler(e, 'POA')}
+              />
+            </div>
             <div className="grid gap-4">
               <label className="text-[1.4rem]">ID Type</label>
               <input
@@ -290,7 +292,10 @@ const PersonalInfo = () => {
               </div>
             </div>
           </div>
-          <button className="text-[1.6rem] bg-color-primary px-10 py-6 justify-self-end w-[28rem] rounded-lg text-color-white uppercase font-semibold self-center" disabled={!isDisabled}>
+          <button
+            className="text-[1.6rem] bg-color-primary px-10 py-6 justify-self-end w-[28rem] rounded-lg text-color-white uppercase font-semibold self-center disabled:opacity-60 disabled:cursor-not-allowed"
+            disabled={isDisabled}
+          >
             Continue
           </button>
         </form>
