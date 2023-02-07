cfd28a2b320acdf623a629cf0f41f9bd9e807d1a
Igboanugwo Collins <munisco12@gmail.com>
2023-02-07 15:54:55 +0100

    working user filled dashboard

--- src/store/features/user/user.ts
+++ src/store/features/user/user.ts
@@ -1,45 +1,42 @@
 import { AppState } from '../../app/store';
 import { createSlice } from '@reduxjs/toolkit';
 
+type ValidatingPath = 'businessInfo' | 'personalInfo' | 'portsAndTerminal';
 export interface IUser {
   firstName: string;
   lastName: string;
   email: string;
+  onboarding: {
+    validating: 'idle' | 'pending' | 'succeeded' | 'failed';
+    error: {
+      path: ValidatingPath;
+      message: string;
+    }[];
+  };
 }
 
 const initialState: IUser = {
   firstName: '',
   lastName: '',
   email: '',
+  onboarding: {
+    validating: 'idle',
+    error: [],
+  },
 };
 
 export const userSlice = createSlice({
   name: 'userState',
   initialState,
   reducers: {
-    updateUser(state, action) {
-      const payload = action.payload;
+    
 
-      console.log(payload);
-
-      const values = Object.entries(payload);
-
-      values.forEach((item) => {
-        const [key, value] = item;
-
-        state[key as keyof IUser] = value as string;
-      });
-    },
-    clearUser(state, action) {
-      console.log('++++++++++++++clearing user');
-      state = initialState;
-    },
   },
 
   extraReducers: (builder) => {},
 });
 
-export const { clearUser, updateUser } = userSlice.actions;
+export const { updateUser } = userSlice.actions;
 
 export const selectUser = (state: AppState) => state.user;
 
