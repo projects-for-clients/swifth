1ec18653b67c4738b4a5c393ef1d1ec84a0ab5ca
Igboanugwo Collins <munisco12@gmail.com>
2023-02-07 17:25:10 +0100

    working user filled dashboard

--- src/container/dashboard/home.tsx
+++ src/container/dashboard/home.tsx
@@ -47,6 +47,111 @@ function home() {
     setIsDropDown({ isPortAndTerminalDown: !isDropDown.isPortAndTerminalDown });
   };
 
+  const firstStep = (
+    <>
+      <p className="text">
+        It's time to set up your account. We will need a few things to get you
+        going
+      </p>
+
+      <div className="grid grid-cols-60-auto gap-4 items-center -pt-8">
+        <div className="progressBar">
+          <progress
+            className="progressBar__item"
+            max="100"
+            value="40"
+          ></progress>
+          <p className="absolute left-4 text-color-tertiary ">
+            <span>0%</span> <span>Completed</span>
+          </p>
+        </div>
+
+        <Link
+          className="text-[1.4rem] font-semibold uppercase text-color-white bg-color-primary rounded-lg h-[4.5rem] px-8 flex items-center justify-center"
+          to="/dashboard/onboarding"
+        >
+          Get Started
+        </Link>
+      </div>
+      <div className="grid gap-4">
+        <div className="flex items-center gap-4">
+          <EllipseSvg />
+          <div
+            className="flex items-center w-full justify-between cursor-pointer "
+            onClick={handleBusinessDrop}
+          >
+            <div>
+              <p className="text">Your business information</p>
+              {isDropDown.isBusinessDown && (
+                <p className="text-[1.4rem] font-light">
+                  Details of your business required
+                </p>
+              )}
+            </div>
+            {isDropDown.isBusinessDown ? (
+              <FiChevronUp className="w-[1.6rem] h-[1.6rem]" />
+            ) : (
+              <FiChevronDown className="w-[1.6rem] h-[1.6rem]" />
+            )}{' '}
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          {/* <CheckmarkSvg /> */}
+          <EllipseSvg />
+
+          <div
+            className="flex items-center w-full justify-between cursor-pointer "
+            onClick={handlePAndTDrop}
+          >
+            <div>
+              <p className="text">Port and terminal Info</p>
+
+              {isDropDown.isPortAndTerminalDown && (
+                <p className="text-[1.4rem] font-light">
+                  Your port and terminal details required
+                </p>
+              )}
+            </div>
+            {isDropDown.isPortAndTerminalDown ? (
+              <FiChevronUp className="w-[1.6rem] h-[1.6rem]" />
+            ) : (
+              <FiChevronDown className="w-[1.6rem] h-[1.6rem]" />
+            )}{' '}
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          {/* <CancelSvg /> */}
+          <EllipseSvg />
+
+          <div
+            className="flex items-center w-full justify-between cursor-pointer "
+            onClick={handleContactDown}
+          >
+            {' '}
+            <div>
+              <p className="text">Contact information</p>
+              {isDropDown.isContactDown && (
+                <p className="text-[1.4rem] font-light">
+                  Your personal Information required
+                </p>
+              )}
+            </div>
+            {isDropDown.isContactDown ? (
+              <FiChevronUp className="w-[1.6rem] h-[1.6rem]" />
+            ) : (
+              <FiChevronDown className="w-[1.6rem] h-[1.6rem]" />
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
+  const secondStep = (
+    <>
+    </>
+  )
+
   return (
     <>
       <Header title="Hello, Nachi" subTitle="Welcome to Swifth" />
@@ -55,101 +160,7 @@ function home() {
         <section className="grid auto-cols-auto">
           <div className="rounded-lg p-4 border border-color-purple-light grid">
             <h2 className="heading3">Account Setup</h2>
-            <p className="text">
-              It's time to set up your account. We will need a few things to get
-              you going
-            </p>
-            
-            <div className="grid grid-cols-60-auto gap-4 items-center -pt-8">
-              <div className="progressBar">
-                <progress
-                  className="progressBar__item"
-                  max="100"
-                  value="40"
-                ></progress>
-                <p className="absolute left-4 text-color-tertiary ">
-                  <span>0%</span> <span>Completed</span>
-                </p>
-              </div>
-             
-              <Link
-                className="text-[1.4rem] font-semibold uppercase text-color-white bg-color-primary rounded-lg h-[4.5rem] px-8 flex items-center justify-center"
-                to="/dashboard/onboarding"
-              >
-                Get Started
-              </Link>
-            </div>
-            <div className="grid gap-4">
-              <div className="flex items-center gap-4">
-                <EllipseSvg />
-                <div
-                  className="flex items-center w-full justify-between cursor-pointer "
-                  onClick={handleBusinessDrop}
-                >
-                  <div>
-                    <p className="text">Your business information</p>
-                    {isDropDown.isBusinessDown && (
-                      <p className="text-[1.4rem] font-light">
-                        Details of your business required
-                      </p>
-                    )}
-                  </div>
-                  {isDropDown.isBusinessDown ? (
-                    <FiChevronUp className="w-[1.6rem] h-[1.6rem]" />
-                  ) : (
-                    <FiChevronDown className="w-[1.6rem] h-[1.6rem]" />
-                  )}{' '}
-                </div>
-              </div>
-              <div className="flex items-center gap-4">
-                {/* <CheckmarkSvg /> */}
-                <EllipseSvg />
-
-                <div
-                  className="flex items-center w-full justify-between cursor-pointer "
-                  onClick={handlePAndTDrop}
-                >
-                  <div>
-                    <p className="text">Port and terminal Info</p>
-
-                    {isDropDown.isPortAndTerminalDown && (
-                      <p className="text-[1.4rem] font-light">
-                        Your port and terminal details required
-                      </p>
-                    )}
-                  </div>
-                  {isDropDown.isPortAndTerminalDown ? (
-                    <FiChevronUp className="w-[1.6rem] h-[1.6rem]" />
-                  ) : (
-                    <FiChevronDown className="w-[1.6rem] h-[1.6rem]" />
-                  )}{' '}
-                </div>
-              </div>
-              <div className="flex items-center gap-4">
-                {/* <CancelSvg /> */}
-                <EllipseSvg />
-
-                <div
-                  className="flex items-center w-full justify-between cursor-pointer "
-                  onClick={handleContactDown}
-                >
-                  {' '}
-                  <div>
-                    <p className="text">Contact information</p>
-                    {isDropDown.isContactDown && (
-                      <p className="text-[1.4rem] font-light">
-                        Your personal Information required
-                      </p>
-                    )}
-                  </div>
-                  {isDropDown.isContactDown ? (
-                    <FiChevronUp className="w-[1.6rem] h-[1.6rem]" />
-                  ) : (
-                    <FiChevronDown className="w-[1.6rem] h-[1.6rem]" />
-                  )}
-                </div>
-              </div>
-            </div>
+            {firstStep}
           </div>
         </section>
         <section className="grid gap-4 self-baseline">
