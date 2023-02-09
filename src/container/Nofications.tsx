732c4e7ed8eb77f36893afbca3749c252ff47f9b
Igboanugwo Collins <munisco12@gmail.com>
2023-02-09 05:23:48 +0100

    working on notifications

--- src/container/Nofications.tsx
+++ src/container/Nofications.tsx
@@ -96,15 +96,18 @@ const QUOTE_REQUESTS: AllNofications[] = [
 ];
 
 export const AllNofications = () => {
-   
-    return (
-        ALL_NOTIFICATIONS.map((item, i) => (
-            <div className="flex justify-between items-center border-b border-color-red-light-1 py-4">
-                <div className="">
-                <p className="text-[1.6rem]">{item.title}</p>
-                <p className="text-[1.2rem] text-gray-500">{item.date}</p>
-                    </div>
-                </div>
-        ))
-    )
-}
+  return ALL_NOTIFICATIONS.map((item, i) => (
+    <div className="flex justify-between items-center border-b border-color-red-light-1 py-4">
+      <div className="">
+        <p className="text-[1.6rem]">{item.title}</p>
+        <p className="text-[1.2rem] text-gray-500">{item.date}</p>
+      </div>
+      {item.amount && (
+        <p className="text-[1.6rem] text-color-red-light-1">
+           <span className="text-gray-500">NGN</span> {item.amount.toLocaleString()}
+        </p>
+
+        )}
+    </div>
+  ));
+};
