e1f375effd46b4800e54be10346b450fe5ae5f2e
Igboanugwo Collins <munisco12@gmail.com>
2023-02-09 14:51:58 +0100

    working on notifications

--- src/container/dashboard/orders.tsx
+++ src/container/dashboard/orders.tsx
@@ -7,13 +7,14 @@ function orders() {
       <Header title="Orders" />
 
       <main>
-        <section className="relative">
+        <section className="relative flex items-center">
           <input
             type="text"
             className="max-w-[45rem] border border-gray-200 rounded-md py-6 pr-3 pl-10"
+            placeholder='search'
           />
 
-          <CiSearch />
+          <CiSearch className='absolute left-3'/>
         </section>
       </main>
     </>
