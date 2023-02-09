98c26bb787105d8b78825eeed32e3db74ae0c271
Igboanugwo Collins <munisco12@gmail.com>
2023-02-09 14:38:47 +0100

    working on notifications

--- src/container/Nofications.tsx
+++ src/container/Nofications.tsx
@@ -175,7 +175,7 @@ export const QuoteRequestsDetails: FC<IQuoteRequestsProps> = ({
           gridTemplateRows: '1.2fr 1fr',
         }}
       >
-        <div className="absolute rounded-3xl bg-green-50 border border-green-300 py-8 px-4 text-green-500 flex w-[36rem] mx-auto top-4 left-0 right-0">
+        <div className="absolute rounded-3xl bg-green-50 border border-green-300 py-8 px-4 text-green-700 flex items-center w-[36rem] mx-auto top-4 left-0 right-0">
          <span className='w-full'>Quote Sent!</span>
          <GrClose/>
         </div>
