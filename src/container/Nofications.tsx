c9a12d550e63ff624537b99b05e5bc1c374a54e7
Igboanugwo Collins <munisco12@gmail.com>
2023-02-09 14:36:56 +0100

    working on notifications

--- src/container/Nofications.tsx
+++ src/container/Nofications.tsx
@@ -1,5 +1,13 @@
-import { Dispatch, FC, useEffect, useState, SetStateAction, FormEvent } from 'react';
+import {
+  Dispatch,
+  FC,
+  useEffect,
+  useState,
+  SetStateAction,
+  FormEvent,
+} from 'react';
 import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+import { GrClose } from 'react-icons/gr';
 import { toast, ToastContainer } from 'react-toastify';
 import { ShowDetails, SwitchPath } from './dashboard/Home';
 
@@ -142,8 +150,6 @@ export const QuoteRequestsDetails: FC<IQuoteRequestsProps> = ({
     setQuoteDetails(data);
   }, [showDetails.id]);
 
-
-
   const goBack = () => {
     setShowDetails({
       show: false,
@@ -158,22 +164,26 @@ export const QuoteRequestsDetails: FC<IQuoteRequestsProps> = ({
     setToDisplay('hidden');
   };
 
-  const handleQuoteSubmit = (e:FormEvent) => {
+  const handleQuoteSubmit = (e: FormEvent) => {
     e.preventDefault();
-
-  
-  }
+  };
   return (
     <>
-      <section className={`absolute top-0 left-0 right-0 bottom-0 grid content-end bg-[#000000ad] ${toDisplay}`} style={{
-        gridTemplateRows: '1.2fr 1fr'
-      }}>
-        <div className='absolute'> 
-        Quote Sent</div>
-        <div onClick={closeModal}>
-            &nbsp;
+      <section
+        className={`absolute top-0 left-0 right-0 bottom-0 grid content-end bg-[#000000ad] ${toDisplay}`}
+        style={{
+          gridTemplateRows: '1.2fr 1fr',
+        }}
+      >
+        <div className="absolute rounded-lg bg-green-50 border border-green-300 py-8 px-4 text-green-500 flex">
+         <span>Quote Sent</span>
+         <GrClose/>
         </div>
-        <form className="bg-white py-10 px-10 rounded-t-3xl" onSubmit={handleQuoteSubmit}>
+        <div onClick={closeModal}>&nbsp;</div>
+        <form
+          className="bg-white py-10 px-10 rounded-t-3xl"
+          onSubmit={handleQuoteSubmit}
+        >
           <p className="text-[1.6rem] text-gray-600 mb-8">Quote Amount</p>
 
           <div>
@@ -241,7 +251,10 @@ export const QuoteRequestsDetails: FC<IQuoteRequestsProps> = ({
           </div>
         </main>
 
-        <button className="flex w-full h-full items-center " onClick={() => setToDisplay('grid')}>
+        <button
+          className="flex w-full h-full items-center "
+          onClick={() => setToDisplay('grid')}
+        >
           <span className="bg-color-primary rounded-lg text-white w-full py-4">
             Send Quote
           </span>
