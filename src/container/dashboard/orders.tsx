a6b94a97b7e728c3226f3e9cb59223ebc158fd56
Igboanugwo Collins <munisco12@gmail.com>
2023-02-09 14:51:02 +0100

    working on notifications

--- src/container/dashboard/orders.tsx
+++ src/container/dashboard/orders.tsx
@@ -8,7 +8,10 @@ function orders() {
 
       <main>
         <section className="relative">
-          <input type="text" className='max-w-[45rem] border border-gray-200' />
+          <input
+            type="text"
+            className="max-w-[45rem] border border-gray-200 rounded-md py-6 px-3"
+          />
 
           <CiSearch />
         </section>
