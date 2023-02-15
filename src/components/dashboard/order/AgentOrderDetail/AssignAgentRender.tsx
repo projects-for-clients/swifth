18683acb1090cd7626a3bb4a02e42a69be6e108d
Igboanugwo Collins <munisco12@gmail.com>
2023-02-15 13:15:43 +0100

    working on orders

--- src/components/dashboard/order/AgentOrderDetail/AssignAgentRender.tsx
+++ src/components/dashboard/order/AgentOrderDetail/AssignAgentRender.tsx
@@ -129,7 +129,7 @@ const AssignAgentRender: FC<AssignAgentRender> = ({
             {agents
               .filter((agent) => agent.toLowerCase().includes(search))
               .map((agent) => (
-                <button className='border-b border-b-color-red-light-1 py-4 text-start'>
+                <button className='border-b rounded-lg border-b-color-red-light-1 py-4 text-start hover:bg-gray-100 hover:translate-x-2 hover:pl-5 transition-all'>
                   <span>{agent}</span>
                 </button>
               ))}
