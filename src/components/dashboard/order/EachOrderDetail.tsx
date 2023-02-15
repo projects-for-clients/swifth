ed6dadde9ccd285360349240879827611ac84910
Igboanugwo Collins <munisco12@gmail.com>
2023-02-15 11:26:58 +0100

    working on orders

--- src/components/dashboard/order/EachOrderDetail.tsx
+++ src/components/dashboard/order/EachOrderDetail.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect, FC, Fragment } from 'react';
+import { useState, useEffect, FC, Fragment, FormEvent } from 'react';
 import { BsArrowLeft } from 'react-icons/bs';
+import { GrClose } from 'react-icons/gr';
 import { DialogType } from '../../../container/dashboard/orders';
 import { OrderHistoryDetail } from './OrderHistory';
 import { InProgress, filterByColors } from './OrdersData';
@@ -34,7 +35,7 @@ const Clearing = () => {
       name: 'Decline',
       className: 'text-red-600',
     },
-  ] as const
+  ] as const;
 
   const clearingDocs = [
     {
@@ -101,31 +102,31 @@ const Clearing = () => {
                 )}
               </p>
               {toggleSortMenu.key === doc.name && (
-                  <div className="absolute top-[6rem] w-[25rem] right-0 shadow-lg bg-white rounded-xl grid gap-2 z-20 capitalize">
-                    {selectFrom.map((item, i) => {
-                      return (
-                        <button
-                          className={`text-[1.4rem] hover:bg-gray-100 p-4 text-left flex items-center gap-4 disabled:opacity-25 disabled:cursor-not-allowed ${
-                            doc.submitted &&
-                            item.name === 'Send submission reminder'
-                              ? 'hidden'
-                              : 'flex'
-                          }`}
-                          key={i}
-                          disabled={
-                            !doc.submitted &&
-                            item.name !== 'Send submission reminder'
-                          }
-                          onClick={() => handleSelectedItem(item.name)}
-                        >
-                          <img src={item.imgUri} alt="" />
-                          <span className={`${item.className} font-medium`}>
-                            {item.name}
-                          </span>
-                        </button>
-                      );
-                    })}
-                  </div>
+                <div className="absolute top-[6rem] w-[25rem] right-0 shadow-lg bg-white rounded-xl grid gap-2 z-20 capitalize">
+                  {selectFrom.map((item, i) => {
+                    return (
+                      <button
+                        className={`text-[1.4rem] hover:bg-gray-100 p-4 text-left flex items-center gap-4 disabled:opacity-25 disabled:cursor-not-allowed ${
+                          doc.submitted &&
+                          item.name === 'Send submission reminder'
+                            ? 'hidden'
+                            : 'flex'
+                        }`}
+                        key={i}
+                        disabled={
+                          !doc.submitted &&
+                          item.name !== 'Send submission reminder'
+                        }
+                        onClick={() => handleSelectedItem(item.name)}
+                      >
+                        <img src={item.imgUri} alt="" />
+                        <span className={`${item.className} font-medium`}>
+                          {item.name}
+                        </span>
+                      </button>
+                    );
+                  })}
+                </div>
               )}
             </div>
           </Fragment>
@@ -203,9 +204,9 @@ const EachOrderDetail: FC<EachOrderDetail> = ({
     history: <History />,
   };
 
-  return (
-    <>
-      <div className=" h-full items-baseline w-[70rem]">
+  const InitialRender = () => {
+    return (
+      <div className=" h-full items-baseline w-[80rem]">
         <div className="flex gap-10 items-center">
           <BsArrowLeft
             className="text-[2.4rem] cursor-pointer"
@@ -328,8 +329,108 @@ const EachOrderDetail: FC<EachOrderDetail> = ({
           </main>
         )}
       </div>
-    </>
-  );
+    );
+  };
+
+  const AssignAgentRender = () => {
+    const [toDisplay, setToDisplay] = useState('hidden');
+    const [toastDisplay, setToastDisplay] = useState('hidden');
+
+    const goBack = () => {
+      // setShowDetails({
+      //   show: false,
+      // });
+      // setCurrentPath('quoteRequests');
+    };
+
+    const closeModal = () => {
+      setToDisplay('hidden');
+    };
+
+    const handleQuoteSubmit = (e: FormEvent) => {
+      e.preventDefault();
+      setToastDisplay('flex');
+    };
+
+    useEffect(() => {
+      if (toastDisplay === 'flex') {
+        setTimeout(() => {
+          setToastDisplay('hidden');
+        }, 3000);
+      }
+    }, [toastDisplay]);
+
+    return (
+      <>
+        <section
+          className={`absolute top-0 left-0 right-0 bottom-0 grid content-end bg-[#000000ad] ${toDisplay}`}
+          style={{
+            gridTemplateRows: '1.2fr 1fr',
+          }}
+        >
+          <div
+            className={`absolute rounded-3xl bg-green-50 border border-green-300 py-8 px-4 text-green-700 flex items-center w-[36rem] mx-auto top-4 left-0 right-0 ${toastDisplay}`}
+          >
+            <span className="w-full text-center">Quote Sent!</span>
+            <GrClose
+              onClick={() => setToastDisplay('hidden')}
+              className="cursor-pointer"
+            />
+          </div>
+          <div onClick={closeModal}>&nbsp;</div>
+          <form
+            className="bg-white py-10 px-10 rounded-t-3xl"
+            onSubmit={handleQuoteSubmit}
+          >
+            <p className="text-[1.6rem] text-gray-600 mb-8">Quote Amount</p>
+
+            <div>
+              <label htmlFor="amount" className="text-[1.4rem] text-gray-600">
+                Enter Amount
+              </label>
+              <input
+                type="number"
+                name="amount"
+                placeholder="Enter Amount"
+                required
+                id="amount"
+                className="w-full bg-gray-100 rounded-md py-6 px-3 outline-none"
+              />
+            </div>
+
+            <button className="flex w-full h-full items-center ">
+              <span className="bg-color-primary rounded-lg text-white w-full py-4">
+                Send Quote
+              </span>
+            </button>
+          </form>
+        </section>
+
+        <div className="px-5 h-full items-baseline ">
+          <div className="flex">
+            <BsArrowLeft
+              className="text-[2.4rem] cursor-pointer"
+              onClick={goBack}
+            />
+            <p className="text-[1.6rem] text-gray-600 w-full text-center">
+              Choose Field Agent
+            </p>
+          </div>
+          <main className="grid gap-4 mt-20"></main>
+
+          <button
+            className="flex w-full h-full items-center "
+            onClick={() => setToDisplay('grid')}
+          >
+            <span className="bg-color-primary rounded-lg text-white w-full py-4">
+              Send Quote
+            </span>
+          </button>
+        </div>
+      </>
+    );
+  };
+  return <></>;
 };
 
 export default EachOrderDetail;
