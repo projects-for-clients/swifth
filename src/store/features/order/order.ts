c7119fe82a888e0a936e08bcaac92acdff58f6da
Igboanugwo Collins <munisco12@gmail.com>
2023-02-16 05:04:36 +0100

    working on orders

--- src/store/features/order/order.ts
+++ src/store/features/order/order.ts
@@ -1,2 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface IOrder {
+  isBOLApproved: boolean;
+}
+
+const initialState:IOrder = {
+  isBOLApproved: false,
+} 
 
 
+export const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {
+    updateOrder: (state, { payload }: { payload: IOrder }) => {
+        return { ...state, ...payload };
+    },
+  },
+});
