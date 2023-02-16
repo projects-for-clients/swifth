c2d8da923994a75310361378efbe55db0037639a
Igboanugwo Collins <munisco12@gmail.com>
2023-02-16 13:14:33 +0100

    working on order

--- src/store/features/order/order.ts
+++ src/store/features/order/order.ts
@@ -56,6 +56,7 @@ export const orderSlice = createSlice({
         RCDocs: [
           ...state.RCDocs.map((doc) => {
             if (doc.name === payload.name) {
+              console.log('update RCDoc', payload)
               return payload;
             }
             return doc;
