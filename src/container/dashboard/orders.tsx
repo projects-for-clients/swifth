a198151ad498a82c1cdd46be13afb00d1b2f527b
Igboanugwo Collins <munisco12@gmail.com>
2023-02-16 14:40:51 +0100

    working on order

--- src/container/dashboard/orders.tsx
+++ src/container/dashboard/orders.tsx
@@ -117,12 +117,13 @@ function orders() {
 
         return setInProgressData(() => [...sortedNames]);
       } else if ((selectedSort as SortBy) === 'Most Recent') {
-        // const sortedDates = INPROGRESS.sort((a, b) => {
-        //   console.log({a, b})
-        //   return new Date(a.date).getTime() - new Date(b.date).getTime();
-        // });
+        const sortedDates = INPROGRESS.sort((a, b) => {
+          console.log({a, b})
+          return new Date(a.date).getTime() - new Date(b.date).getTime();
+        });
 
-        // return setInProgressData(() => [...sortedDates]);
+        return setInProgressData(() => [...sortedDates]);
+        
       }
     }
   }, [selectedSort]);
