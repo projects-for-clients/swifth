f46773db9741f1701b8c19f5dbf8241892466729
Igboanugwo Collins <munisco12@gmail.com>
2023-02-17 14:32:32 +0100

    working on order

--- src/components/dashboard/order/OrdersData.tsx
+++ src/components/dashboard/order/OrdersData.tsx
@@ -360,7 +360,6 @@ export const InProgressView: FC<{
   inProgressData: InProgress[];
   openOrderDetail: (item: InProgress) => void;
 }> = ({ inProgressData, openOrderDetail }) => {
-  console.log({ inProgressData });
   return (
     <div
       className="grid mt-[5rem] gap-10"
