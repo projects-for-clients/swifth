b76eb12212b770404021897a87dfba070657333f
Igboanugwo Collins <munisco12@gmail.com>
2023-02-18 19:25:42 +0100

    refactored order redux logic

--- src/components/dashboard/order/AgentOrderDetail/EachOrderDetailPath.tsx
+++ src/components/dashboard/order/AgentOrderDetail/EachOrderDetailPath.tsx
@@ -33,7 +33,6 @@ export const AgentClearing: FC<AgentClearing> = ({
     docType?: 'RCDoc' | 'ClearingDoc';
     display: 'hidden' | 'grid'
   }>({
-    docType: null,
     display: 'hidden',
   });
 
