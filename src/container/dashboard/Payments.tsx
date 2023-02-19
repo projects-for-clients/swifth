f4c18681c882b8c7bf84f6b56d21ac296ef47dd5
Igboanugwo Collins <munisco12@gmail.com>
2023-02-19 19:30:31 +0100

    working on payments

--- src/container/dashboard/Payments.tsx
+++ src/container/dashboard/Payments.tsx
@@ -161,8 +161,8 @@ function Payments() {
 
                 <section className="">
                   <div className="mt-10 mb-20 h-[50rem]">
-                    <details onToggle={accordionHandler}>
-                      <summary>Epcot Center</summary>
+                    <details onToggle={accordionHandler} className='details'>
+                      <summary >Epcot Center</summary>
                       <p>
                         Epcot is a theme park at Walt Disney World Resort
                         featuring exciting attractions, international pavilions,
