10ea74edae34ca7f75963de3143d21043dadc7c1
Igboanugwo Collins <munisco12@gmail.com>
2023-02-19 17:53:19 +0100

    working on payments

--- src/container/dashboard/Payments.tsx
+++ src/container/dashboard/Payments.tsx
@@ -55,8 +55,6 @@ function Payments() {
 
   const [paymentsData, setPaymentsData] = useState<Payments[]>(PAYMENTS);
 
-  console.log({paymentsData, PAYMENTS})
-
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setSearch(value);
@@ -114,10 +112,93 @@ function Payments() {
           </figure>
 
           <section className="h-full">
-              {/* <EachOrderDetail
-                orderDetail={OrderDetail}
-                handleCloseDialog={() => handleCloseDialog()}
-              /> */}
+            <div className=" h-full items-baseline w-[80rem] overflow-y-scroll pb-10">
+            
+              <main className="grid gap-10 mt-10  ">
+                <div className="grid justify-start justify-items-start gap-4">
+                  <p className="text-[2rem] text-gray-600 text-center">
+                    {adminName}
+                  </p>
+                  <p
+                    className={`py-1.5 px-8 rounded-2xl text-white ${
+                      filterByColors[tag!].bg
+                    } ${filterByColors[tag!].text}`}
+                  >
+                    {tag}
+                  </p>{' '}
+                </div>
+                <section
+                  className="grid gap-10"
+                  style={{
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(15rem, 1fr))',
+                  }}
+                >
+                  <div>
+                    <p className=" text-gray-400">Agent</p>
+                    <p className=" text-gray-600">{agentName}</p>
+                  </div>
+                  <div>
+                    <p className=" text-gray-400">Date</p>
+                    <p className=" text-gray-600">
+                      {date?.toLocaleString('en-GB', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                      })}
+                    </p>
+                  </div>
+                  <div>
+                    <p className=" text-gray-400">Amount Paid</p>
+                    <p className=" text-gray-600">
+                      {amountPaid?.toLocaleString('en-GB', {
+                        style: 'currency',
+                        currency: 'NGN',
+                      })}
+                    </p>
+                  </div>
+                  <div>
+                    <p className=" text-gray-400">Total Amount</p>
+                    <p className=" text-gray-600">
+                      {totalAmount?.toLocaleString('en-GB', {
+                        style: 'currency',
+                        currency: 'NGN',
+                      })}
+                    </p>{' '}
+                  </div>
+                </section>
+                <section
+                  className="grid gap-10 border border-color-purple-light p-8 rounded-2xl"
+                  style={{
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(15rem, 1fr))',
+                  }}
+                >
+                  <div>
+                    <p className=" text-gray-400">Car Brand</p>
+                    <p className=" text-gray-600">{carBrand}</p>
+                  </div>
+                  <div>
+                    <p className=" text-gray-400">Car Year</p>
+                    <p className=" text-gray-600">{carYear}</p>
+                  </div>
+                  <div>
+                    <p className=" text-gray-400">Car Model</p>
+                    <p className=" text-gray-600">{carModel}</p>
+                  </div>
+                  <div>
+                    <p className=" text-gray-400">Car Trim</p>
+                    <p className=" text-gray-600">{carTrim}</p>
+                  </div>
+                </section>
+
+                <section className="">
+                
+
+                  <div className="mt-10 mb-20 h-[50rem]">
+                    
+                  </div>
+                </section>
+              </main>
+            </div>
           </section>
         </div>
       </dialog>
@@ -189,6 +270,43 @@ function Payments() {
                     />
                   )}
                 </>
+              </div>
+
+              <div
+                className="grid mt-[5rem] gap-10"
+                style={{
+                  gridTemplateColumns: 'repeat(auto-fit, minmax(33rem, 1fr))',
+                }}
+              >
+                {paymentsData.map((item, i) => {
+                  const { name, description, date, tag } = item;
+
+                  return (
+                    <div
+                      className="p-8 bg-white rounded-3xl border border-color-purple-light-2 cursor-pointer"
+                      key={i}
+                      onClick={() => handleOpenDialog()}
+                    >
+                      <div>
+                        <p className="text-[1.6rem]">{name}</p>
+                        <p className="text-[1.4rem] whitespace-nowrap text-ellipsis overflow-hidden text-gray-500 max-w-[20rem]">
+                          {description}
+                        </p>
+                      </div>
+
+                      <div className="text-[1.2rem] flex items-center justify-between pt-8">
+                        <p className="text-gray-500">
+                          {date.toLocaleString('en-GB', {
+                            day: 'numeric',
+                            month: 'short',
+                            year: 'numeric',
+                          })}
+                        </p>
+                        <p className={`py-1.5 px-4 rounded-2xl `}>{tag}</p>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </Fragment>
           )}
