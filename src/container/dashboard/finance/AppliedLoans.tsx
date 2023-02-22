19528421971a7339e740828dcce9c59cfdf9ed07
Igboanugwo Collins <munisco12@gmail.com>
2023-02-22 11:30:39 +0100

    working on finance

--- src/container/dashboard/finance/AppliedLoans.tsx
+++ src/container/dashboard/finance/AppliedLoans.tsx
@@ -24,24 +24,6 @@ const AppliedLoans = () => {
             onClick={() => handleOpenDialog(item)}
           >
             <div className="text-[1.2rem] flex items-center justify-between">
-              <p
-                className={`py-1.5 px-4 rounded-2xl ${
-                  tag === 'Fully Paid'
-                    ? 'bg-color-primary text-white'
-                    : 'text-black bg-[#FAC772]'
-                }`}
-              >
-                {tag}
-              </p>
-              <p className="text-gray-500">
-                {date.toLocaleString('en-GB', {
-                  day: 'numeric',
-                  month: 'short',
-                  year: 'numeric',
-                })}
-              </p>
-            </div>
-            <div className="flex justify-between items-end">
               <div>
                 <p className="text-[1.6rem]">{name}</p>
                 <p className="text-[1.4rem] whitespace-nowrap text-ellipsis overflow-hidden text-gray-500 max-w-[20rem]">
@@ -49,10 +31,19 @@ const AppliedLoans = () => {
                 </p>
               </div>
 
-              <p className="text-color-purple-1 flex gap-1 font-medium">
-                <span className="text-gray-500">NGN</span>
-                {amount.toLocaleString()}
-              </p>
+              <div className=" justify-between items-end">
+                <p className="text-gray-500">
+                  {date.toLocaleString('en-GB', {
+                    day: 'numeric',
+                    month: 'short',
+                    year: 'numeric',
+                  })}
+                </p>
+                <p className="text-color-purple-1 flex gap-1 font-medium">
+                  <span className="text-gray-500">NGN</span>
+                  {amount.toLocaleString()}
+                </p>
+              </div>
             </div>
           </div>
         );
