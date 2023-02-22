211394693896c07e36ae2e6b7cabe8fece2b28bb
Igboanugwo Collins <munisco12@gmail.com>
2023-02-22 17:20:23 +0100

    working on payout bank

--- src/pages/PayoutBank.tsx
+++ src/pages/PayoutBank.tsx
@@ -3,16 +3,11 @@ import Header from '../components/dashboard/Header';
 
 export const CreateFinanceContext = createContext(null as any);
 
-
-
 function PayoutBank() {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const [currentPath, setCurrentPath] = useState()
-  const [addedBank, setAddedBank] = useState(true)
-
-
-
+  const [currentPath, setCurrentPath] = useState();
+  const [addedBank, setAddedBank] = useState(true);
 
   return (
     <CreateFinanceContext.Provider value={null}>
@@ -24,11 +19,13 @@ function PayoutBank() {
       </dialog>
       <main>
         {addedBank ? (
-          <div className="flex items-center gap-8 border border-[#BEB3DE] p-8 rounded-3xl">
-            <img src="/icons/payoutBank.svg" alt="" className="w-[8rem]" />
-            <div className=" text-gray-500 flex items-center gap-4">
-              <img src="/icons/add-circle.svg" alt="" className="w-[3rem]" />
-              <p className="text-[1.4rem]">Add Payout Bank</p>
+          <div className="flex items-center border border-[#BEB3DE] p-8 rounded-3xl justify-between">
+            <div className="flex items-center gap-8">
+              <img src="/icons/payoutBank.svg" alt="" className="w-[8rem]" />
+              <div className=" text-gray-500 flex items-center gap-4">
+                <img src="/icons/add-circle.svg" alt="" className="w-[3rem]" />
+                <p className="text-[1.4rem]">Add Payout Bank</p>
+              </div>
             </div>
           </div>
         ) : (
