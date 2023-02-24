8aaa85d1365b2e48118925471e429606d8ce0568
Igboanugwo Collins <munisco12@gmail.com>
2023-02-24 17:58:17 +0100

    working on delivery

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -164,7 +164,7 @@ function Delivery() {
         }}
       >
         <Header title="Delivery" />
-        <DeliveryHistory openHistoryDialog={openHistoryDialog} />
+        <DeliveryHistory openHistoryDialog={openHistoryDialog} setOpenHistoryDialog={setOpenHistoryDialog} />
         <main className="text-[1.6rem]">
           <section className="relative flex items-center w-[45rem] mx-auto">
             <input
