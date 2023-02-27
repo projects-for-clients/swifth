6c414845f406047413eaf8ab0168981ff6eebf16
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 12:15:19 +0100

    working on team

--- src/pages/team/ViewTeam.tsx
+++ src/pages/team/ViewTeam.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { BiArrowBack } from 'react-icons/bi'
+import { useLocation } from 'react-router-dom'
 import Header from '../../components/dashboard/Header'
 
 const ViewTeam = () => {
     const location = useLocation()
+
+    console.log({location})
+    const {id, name, role} = location && location?.state 
   return (
     <>
       <Header title="Team" />
