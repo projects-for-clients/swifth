ea0fa915f5f6b838b5b48e0d753ce3388f59baa4
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 12:13:22 +0100

    working on team

--- src/pages/team/ViewTeam.tsx
+++ src/pages/team/ViewTeam.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BiArrowBack } from 'react-icons/bi'
 import Header from '../../components/dashboard/Header'
 
 const ViewTeam = () => {
@@ -8,7 +9,7 @@ const ViewTeam = () => {
 
       <main className="text-[1.6rem] grid gap-10">
 
-        
+        <BiArrowBack/>
         </main>
         </>
   )
