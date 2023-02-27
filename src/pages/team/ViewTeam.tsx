ca536dd6c4e28e781d56d0b07a8a4b3b16d3cc7f
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 12:42:18 +0100

    working on team

--- src/pages/team/ViewTeam.tsx
+++ src/pages/team/ViewTeam.tsx
@@ -44,7 +44,7 @@ const ViewTeam = () => {
           </div>
         </div>
 
-        <div className="grid gap-8 border-t max-w-[70rem] py-16">
+        {/* <div className="grid gap-8 border-t max-w-[70rem] py-16">
           <div className="grid grid-cols-2">
             <div>
               <p className="text-[1.4rem] text-gray-500">Role</p>
@@ -71,6 +71,10 @@ const ViewTeam = () => {
             <p className="text-[1.4rem] text-gray-500">Last Interaction</p>
             <p className="capitalize">Dec 3, 2023</p>
           </div>
+        </div> */}
+
+        <div className='grid gap-8 max-w-[70rem] py-16'>
+            <p className='font-medium text-[2rem]'>User type/Role</p>
         </div>
       </main>
 
