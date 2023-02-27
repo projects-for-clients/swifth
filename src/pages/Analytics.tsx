3547706619b69a9f1c45470f205d553bc663bd13
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 16:33:52 +0100

    working on analytics

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,28 +1,10 @@
-import Header from '../../components/dashboard/Header';
+import Header from "../components/dashboard/Header";
 
 function Analytics() {
-  const navigate = useNavigate();
-  const [search, setSearch] = useState('');
-
-  const [teamData, setTeamData] = useState<Team[]>(teamArr);
-
-  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setSearch(value);
-
-    const filtered = teamArr.filter((item) =>
-      item.name.toLowerCase().includes(value.toLowerCase())
-    );
-    setTeamData(filtered);
-  };
-
-  const openMemberDetails = (item: Team) => {
-    navigate(`/dashboard/team/${item.id}`, { state: item });
-  };
 
   return (
     <>
-      <Header title="Team" />
+      <Header title="Analytics" />
 
   
     </>
