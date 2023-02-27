758c417e59e1a94504830e0f64c55f2a18babcdd
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 12:16:40 +0100

    working on team

--- src/pages/team/ViewTeam.tsx
+++ src/pages/team/ViewTeam.tsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { BiArrowBack } from 'react-icons/bi'
-import { useLocation } from 'react-router-dom'
-import Header from '../../components/dashboard/Header'
+import React from 'react';
+import { BiArrowBack } from 'react-icons/bi';
+import { useLocation } from 'react-router-dom';
+import Header from '../../components/dashboard/Header';
 
 const ViewTeam = () => {
-    const location = useLocation()
+  const location = useLocation();
 
-    const {id, name, role} = location && location?.state 
+  const { id, name, role } = location && location?.state;
   return (
     <>
       <Header title="Team" />
 
       <main className="text-[1.6rem] grid gap-10">
-        <div>
-
-        <BiArrowBack/>
-        <p className='text-[2rem]'>{name}</p>
+        <div className="flex items-center gap-8">
+          <BiArrowBack />
+          <p className="text-[2rem]">{name}</p>
         </div>
-        </main>
-        </>
-  )
-}
+      </main>
+    </>
+  );
+};
 
-export default ViewTeam
+export default ViewTeam;
