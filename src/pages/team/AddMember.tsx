afe057e1602727ce9befe2d3abc1a28e6f05bfd8
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 13:34:09 +0100

    working on add team member

--- src/pages/team/AddMember.tsx
+++ src/pages/team/AddMember.tsx
@@ -14,108 +14,106 @@ const AddMember = () => {
     <>
       <Header title="Team" />
 
-      <main className="text-[1.6rem] grid gap-10">
+      <main className="text-[1.6rem] grid gap-10 max-w-[70rem]">
         <div className="flex items-center gap-16">
           <BiArrowBack
             className="text-[1.8rem] cursor-pointer"
-            onClick={
-             () => navigate(-1)
-            }
+            onClick={() => navigate(-1)}
           />
           <p className="text-[2rem]">Add a team Member</p>
         </div>
 
-       
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            className=" border border-gray-300 p-6 outline-none w-full rounded-3xl"
+            placeholder="Enter email address"
+          
+          />{' '}
+        </div>
 
-       
-          <div className="grid gap-8 max-w-[70rem] py-16">
-            <p className="font-medium text-[2rem]">User type/Role</p>
+        <div className="grid gap-8  py-16">
+          <p className="font-medium text-[2rem]">User type/Role</p>
 
-            <div className="grid gap-8">
-              <label
-                htmlFor="admin"
-                className="flex items-center gap-8 cursor-pointer"
-              >
-                <input
-                  type="radio"
-                  name="role"
-                  id="admin"
-                  className="hidden"
-                  onChange={() => setCurrentRole('admin')}
-                />
+          <div className="grid gap-8">
+            <label
+              htmlFor="admin"
+              className="flex items-center gap-8 cursor-pointer"
+            >
+              <input
+                type="radio"
+                name="role"
+                id="admin"
+                className="hidden"
+                onChange={() => setCurrentRole('admin')}
+              />
 
-                {currentRole === 'admin' ? (
-                  <img src="/icons/filled-Role.svg" alt="" />
-                ) : (
-                  <img src="/icons/empty-Role.svg" alt="" />
-                )}
+              {currentRole === 'admin' ? (
+                <img src="/icons/filled-Role.svg" alt="" />
+              ) : (
+                <img src="/icons/empty-Role.svg" alt="" />
+              )}
 
-                <div>
-                  <p className="text-[2rem] font-medium">Admin</p>
-                  <p>Account User with full access</p>
-                </div>
-              </label>
+              <div>
+                <p className="text-[2rem] font-medium">Admin</p>
+                <p>Account User with full access</p>
+              </div>
+            </label>
 
-              <label
-                htmlFor="field agent"
-                className="flex items-center gap-8 cursor-pointer"
-              >
-                <input
-                  type="radio"
-                  name="role"
-                  id="field agent"
-                  className="hidden"
-                  onChange={() => setCurrentRole('field agent')}
-                />
+            <label
+              htmlFor="field agent"
+              className="flex items-center gap-8 cursor-pointer"
+            >
+              <input
+                type="radio"
+                name="role"
+                id="field agent"
+                className="hidden"
+                onChange={() => setCurrentRole('field agent')}
+              />
 
-                {currentRole === 'field agent' ? (
-                  <img src="/icons/filled-Role.svg" alt="" />
-                ) : (
-                  <img src="/icons/empty-Role.svg" alt="" />
-                )}
-                <div>
-                  <p className="text-[2rem] font-medium">Field Agent</p>
-                  <p>
-                    Has full access except, user management and list exports
-                  </p>
-                </div>
-              </label>
+              {currentRole === 'field agent' ? (
+                <img src="/icons/filled-Role.svg" alt="" />
+              ) : (
+                <img src="/icons/empty-Role.svg" alt="" />
+              )}
+              <div>
+                <p className="text-[2rem] font-medium">Field Agent</p>
+                <p>Has full access except, user management and list exports</p>
+              </div>
+            </label>
 
-              <label
-                htmlFor="CustomerRep"
-                className="flex items-center gap-8 cursor-pointer"
-              >
-                <input
-                  type="radio"
-                  name="role"
-                  id="CustomerRep"
-                  className="hidden"
-                  onChange={() => setCurrentRole('customer care rep')}
-                />
+            <label
+              htmlFor="CustomerRep"
+              className="flex items-center gap-8 cursor-pointer"
+            >
+              <input
+                type="radio"
+                name="role"
+                id="CustomerRep"
+                className="hidden"
+                onChange={() => setCurrentRole('customer care rep')}
+              />
 
-                {currentRole === 'customer care rep' ? (
-                  <img src="/icons/filled-Role.svg" alt="" />
-                ) : (
-                  <img src="/icons/empty-Role.svg" alt="" />
-                )}
-                <div>
-                  <p className="text-[2rem] font-medium">
-                    Customer Care Representative
-                  </p>
-                  <p>
-                    Has full access except, user management and list exports
-                  </p>
-                </div>
-              </label>
-            </div>
+              {currentRole === 'customer care rep' ? (
+                <img src="/icons/filled-Role.svg" alt="" />
+              ) : (
+                <img src="/icons/empty-Role.svg" alt="" />
+              )}
+              <div>
+                <p className="text-[2rem] font-medium">
+                  Customer Care Representative
+                </p>
+                <p>Has full access except, user management and list exports</p>
+              </div>
+            </label>
           </div>
-       
+        </div>
 
-        
-          <button className="btn border border-color-primary rounded-2xl uppercase text-color-primary w-[30rem] absolute right-0 bottom-0 mx-8 my-8 font-medium">
-            Save Changes
-          </button>
-      
+        <button className="btn border border-color-primary rounded-2xl uppercase text-color-primary w-[30rem] absolute right-0 bottom-0 mx-8 my-8 font-medium">
+          Send Invite
+        </button>
       </main>
     </>
   );
