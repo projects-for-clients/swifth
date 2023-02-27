0235df076e04d6b4b5ec7280445a87303b64077f
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 17:33:39 +0100

    working on analytics

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,14 +1,58 @@
 import Header from '../components/dashboard/Header';
 import SelectInput from '../components/utils/SelectInput';
 
+
+interface Carousel {
+    title: string;
+    imgUri: string;
+    count: number;
+
+}
+const carousel: Carousel[] = [
+    {
+        title: 'Total Quotes',
+        imgUri: '/icons/analytics/total-quotes.svg',
+        count: 496
+    },
+    {
+        title: 'Total Quotes',
+        imgUri: '/icons/analytics/totalQuotes.svg',
+        count: 496
+    },
+    {
+        title: 'Total Quotes',
+        imgUri: '/icons/analytics/totalQuotes.svg',
+        count: 496
+    },
+    {
+        title: 'Total orders',
+        imgUri: '/icons/analytics/totalOrders.svg',
+        count: 300
+    },
+    {
+        title: 'Total Quotes',
+        imgUri: '/icons/analytics/totalQuotes.svg',
+        count: 496
+    },
+    {
+        title: 'Total Quotes',
+        imgUri: '/icons/analytics/totalQuotes.svg',
+        count: 496
+    },
+    {
+        title: 'Total Quotes',
+        imgUri: '/icons/analytics/totalQuotes.svg',
+        count: 496
+    },
+    {
+        title: 'Total Quotes',
+        imgUri: '/icons/analytics/totalQuotes.svg',
+        count: 496
+    },
+]
 function Analytics() {
-    
 
-        const carousel = [
-            {
 
-            }
-        ]
   return (
     <>
       <Header title="Analytics" />
