3ef267e4692ba47a6d4a4ad72b6855e8190bf864
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 12:19:08 +0100

    working on team

--- src/pages/team/ViewTeam.tsx
+++ src/pages/team/ViewTeam.tsx
@@ -7,15 +7,19 @@ const ViewTeam = () => {
   const location = useLocation();
 
   const { id, name, role } = location && location?.state;
+
+  let initials = name.split(' ').map((n: string[]) => n[0]);
   return (
     <>
       <Header title="Team" />
 
       <main className="text-[1.6rem] grid gap-10">
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-16">
           <BiArrowBack />
           <p className="text-[2rem]">{name}</p>
         </div>
+        {initials}
+        <div></div>
       </main>
     </>
   );
