f7eb98f7602ba4469310febbdb3382d62fb96c9a
Igboanugwo Collins <munisco12@gmail.com>
2023-02-27 12:16:10 +0100

    working on team

--- src/pages/team/ViewTeam.tsx
+++ src/pages/team/ViewTeam.tsx
@@ -6,15 +6,17 @@ import Header from '../../components/dashboard/Header'
 const ViewTeam = () => {
     const location = useLocation()
 
-    console.log({location})
     const {id, name, role} = location && location?.state 
   return (
     <>
       <Header title="Team" />
 
       <main className="text-[1.6rem] grid gap-10">
+        <div>
 
         <BiArrowBack/>
+        <p className='text-[2rem]'>{name}</p>
+        </div>
         </main>
         </>
   )
