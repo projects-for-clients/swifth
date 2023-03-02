a930e8345cbb6e131e6bdab43508b8737546ea50
Igboanugwo Collins <munisco12@gmail.com>
2023-03-02 11:38:25 +0100

    working on reports

--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -144,7 +144,7 @@ const Account = () => {
             </div>
           </div>
           <div className='mt-10'>
-            <p className='border border-color-purple-light rounded-2xl py-4 px-8 flex items-center justify-between'>
+            <p className='border border-color-purple-light-2 rounded-2xl py-8 px-8 flex items-center justify-between'>
               <span>Personal Information</span>
               <span>
                 <HiOutlineChevronRight />
