e761779d4165d18fcba5c19e08d94e16c9e065a8
Igboanugwo Collins <munisco12@gmail.com>
2023-03-02 12:26:54 +0100

    working on account

--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -61,7 +61,7 @@ const Account = () => {
           <>
             <p className="text-[2rem] mb-16">Personal Information</p>
 
-            <div className='grid justify-center'>
+            <div className="grid justify-center">
               <figure className="relative grid place-content-center w-[13rem] h-[13rem]">
                 <img
                   src=""
@@ -72,16 +72,31 @@ const Account = () => {
                   <span>{initials}</span>
                 </p>
 
-                <img src="/icons/camera.svg" alt="" className='absolute right-0 z-20 bottom-0 mb-3' />
+                <img
+                  src="/icons/camera.svg"
+                  alt=""
+                  className="absolute right-0 z-20 bottom-0 mb-3"
+                />
               </figure>
 
-              <div className='text-center mt-10'>
+              <div className="text-center mt-10">
                 <p className="text-[3rem]">{name}</p>
-                <button
-                  className="btn uppercase border border-color-primary text-color-primary text-center rounded-2xl font-medium"
-                >
+                <button className="btn uppercase border border-color-primary text-color-primary text-center rounded-2xl font-medium">
                   Edit Profile
                 </button>
+              </div>
+            </div>
+
+            <div>
+              <p>090238232323</p>
+              <p className="lowercase">{name.split(' ').join('')}@gmail.com</p>
+
+              <div>
+                <p>Contact Person ID card</p>
+                <p className='bg-green-100 border border-color-primary rounded-2xl flex items-center gap-4 text-color-primary'>
+                    <span><IoMdCheckmark/></span>
+                    <span className='text-[1.2rem]'>Verified</span>
+                </p>
               </div>
             </div>
           </>
