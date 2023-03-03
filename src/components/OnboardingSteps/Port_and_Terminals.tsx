a92504f538e8896c0ba0caa0107705737419de67
Igboanugwo Collins <munisco12@gmail.com>
2023-03-03 10:54:23 +0100

    working on account

--- src/components/OnboardingSteps/Port_and_Terminals.tsx
+++ src/components/OnboardingSteps/Port_and_Terminals.tsx
@@ -170,7 +170,6 @@ const Terminal: FC<ITerminal> = ({
   };
 
   if (setup) {
-    console.log('mounted', onboardingInputs.portsAndTerminal.terminalList[id]);
     useEffect(() => {
       if (!loaded) {
 
