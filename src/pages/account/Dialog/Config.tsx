253cdf29835637d8251c29f1f5c398c231da680e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-03 16:54:49 +0100

    working on account

--- src/pages/account/Dialog/Config.tsx
+++ src/pages/account/Dialog/Config.tsx
@@ -194,6 +194,90 @@ function Config({
             }}
           >
             {Array.from({ length: 20 }, (_, i) => (
+              <p key={i} className="border bg-gray-100 rounded-3xl p-6" onClick={
+                moveToCarModels
+              }>
+                Honda
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+  const SurfaceDutyItem = (
+    <>
+      <div className="flex items-center gap-16">
+        <BiArrowBack
+          className="text-[1.8rem] cursor-pointer"
+          onClick={backToDutyCalculation}
+        />
+        <p className="text-[2rem]"> Surface Duty </p>
+      </div>
+
+      <div className="grid gap-16 mt-[5rem]">
+        <div className="relative flex items-center w-[45rem] mx-auto">
+          <input
+            type="text"
+            className=" border border-gray-300 py-6 pr-3 pl-[4rem] outline-none w-full rounded-3xl"
+            placeholder="Search"
+          />
+
+          <img
+            src="/icons/search-normal.svg"
+            alt=""
+            className="absolute left-6 text-[1.8rem]"
+          />
+        </div>
+
+        <div>
+          <p className="font-medium mb-5">Most popular</p>
+          <div
+            className="grid gap-8"
+            style={{
+              gridTemplateColumns: 'repeat(auto-fit, minmax(30rem, 1fr))',
+            }}
+          >
+            {Array.from({ length: 6 }, (_, i) => (
+              <p key={i} className="border bg-gray-100 rounded-3xl p-6">
+                Lexus LX 570, 2019
+              </p>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 mt-10">
+          {filter.map((item) => (
+            <div key={item}>
+              <input
+                type="radio"
+                name="filter"
+                id={item}
+                className="hidden"
+                onChange={() => setSelectedFilter(item)}
+              />
+              <label
+                htmlFor={item}
+                className={`${
+                  item === selectedFilter
+                    ? 'bg-color-primary text-white border-none'
+                    : 'border'
+                } rounded-3xl p-4 w-[10rem] cursor-pointer flex justify-center`}
+              >
+                {item}
+              </label>
+            </div>
+          ))}
+        </div>
+        <div className='mb-[5rem]'>
+          <p className="font-medium mb-5">All Brands</p>
+          <div
+            className="grid gap-8"
+            style={{
+              gridTemplateColumns: 'repeat(auto-fit, minmax(30rem, 1fr))',
+            }}
+          >
+            {Array.from({ length: 20 }, (_, i) => (
               <p key={i} className="border bg-gray-100 rounded-3xl p-6">
                 Honda
               </p>
