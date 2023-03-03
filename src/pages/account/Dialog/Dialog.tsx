dd4cab6cc8a29117fce67016591594c2d4855426
Igboanugwo Collins <munisco12@gmail.com>
2023-03-03 05:28:56 +0100

    working on account

--- src/pages/account/Dialog/Dialog.tsx
+++ src/pages/account/Dialog/Dialog.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { DialogContext, DialogType } from '../Account';
 import Business from './Business';
 import Personal from './Personal';
@@ -6,6 +6,7 @@ import Ports from './Ports';
 
 function Dialog() {
   const { dialogType, setDialogType } = useContext(DialogContext);
+  const [width, setWidth] = useState('w-[50rem]');
 
   const dialogRef = useRef<HTMLDialogElement>(null);
 
@@ -32,7 +33,9 @@ function Dialog() {
 
   return (
     <dialog className="dialog relative text-[1.6rem]" ref={dialogRef}>
-      <div className="bg-white fixed right-0 h-[100vh] w-[50rem] py-4 px-12 overflow-y-scroll">
+      <div
+        className={`bg-white fixed right-0 h-[100vh] py-4 px-12 overflow-y-scroll ${width}`}
+      >
         <input type="text" className="absolute top-0 w-0" />
         <figure className="flex justify-end">
           <img
