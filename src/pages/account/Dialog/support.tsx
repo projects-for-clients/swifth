146a8280d5ccb77909eccd48cd84986377a3435b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-05 19:32:41 +0100

    working on account

--- src/pages/account/Dialog/support.tsx
+++ src/pages/account/Dialog/support.tsx
@@ -6,15 +6,24 @@ function support({closeDialog}: {
  
    return (
      <div className="relative h-[90vh]">
-     
        <>
          <p className="text-[2rem] mb-16">Support</p>
 
          <div className="grid gap-8 mt-[5rem]">
+           <div className='flex items-center gap-8'>
            
+               <img src="/icons/headPhones.svg" alt="" />
+               <p>+234 816727299020</p>
+          
+           </div>
+           <div className='flex items-center gap-8'>
+           
+               <img src="/icons/emailSupport.svg" alt="" />
+               <p>Support@swifth.com</p>
+          
+           </div>
          </div>
        </>
-       
      </div>
    );
 }
