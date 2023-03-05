5a06e74a5e1a0ae4fc2289fbfd916c8277f1caa8
Igboanugwo Collins <munisco12@gmail.com>
2023-03-05 19:36:11 +0100

    working on account

--- src/pages/account/Dialog/support.tsx
+++ src/pages/account/Dialog/support.tsx
@@ -8,10 +8,10 @@ function Support({closeDialog}: {
        <>
          <p className="text-[2rem] mb-16">Supportsfsf</p>
 
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit enim suscipit, accusantium mollitia nulla repellat magnam aspernatur perferendis sed, assumenda itaque incidunt quasi molestias ea illum tenetur, vitae rem animi.
+
          <div className="grid gap-8 mt-[5rem]">
            <div className="flex items-center gap-8">
-             <img src="/icons/headPhones.svg" alt="" />
+             <img src="/icons/headPhone.svg" alt="" />
              <p>+234 816727299020</p>
            </div>
            <div className="flex items-center gap-8">
