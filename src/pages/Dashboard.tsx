3387381c3b52c4571609373c54dbec52b6282935
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 16:24:04 +0100

    working on ref add sos

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ function Dashboard() {
   return (
     <div className=" grid grid-cols-none sm:grid-cols-max-auto h-screen">
       <Sidebar />
-      <div className="dashboard__container">
+      <div className="py-[2rem] px-[4rem] h-screen overflow-y-scroll">
         <Outlet />
       </div>
     </div>
