bf0ffc9f471f233aab1716a440ed7468a7c5bec9
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 15:19:43 +0100

    working on ref add sos

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from 'react';
 import NotificationSvg from '../icons/notificationSvg';
 import AccountSvg from '../icons/sidebar/accountSvg';
+import { BiMenu } from 'react-icons/bi';
 
 type THeader = {
   title: string;
@@ -10,20 +11,24 @@ type THeader = {
 };
 
 function Header({ title, subTitle, onboarding, openDialog }: THeader) {
-
   const handleToggle = () => {
     openDialog && openDialog();
   };
   return (
     <div className="header">
       <div className="header__left">
-        <p className="font-medium text-[2.4rem]">{title}</p>
-        <p className={`${onboarding ? 'text-[1.4rem]' : 'font-medium'}`}>
-          {subTitle}
-        </p>
+        <label htmlFor="menu__checkbox">
+          <BiMenu />
+        </label>
+        <div>
+          <p className="font-medium text-[2.4rem]">{title}</p>
+          <p className={`${onboarding ? 'text-[1.4rem]' : 'font-medium'}`}>
+            {subTitle}
+          </p>
+        </div>
       </div>
       <div className="header__right">
-        <span onClick={handleToggle} className='cursor-pointer'>
+        <span onClick={handleToggle} className="cursor-pointer">
           <NotificationSvg />
         </span>
         <AccountSvg fill="black" />
