0efc2092b202c8b6088355ebb627624067b8a14b
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 19:09:00 +0100

    working on ref add sos

--- src/components/icons/IconsBox.tsx
+++ src/components/icons/IconsBox.tsx
@@ -73,6 +73,9 @@ function IconsBox() {
   return (
     <ul className="grid gap-4 w-full">
       {LINKS.map(({ to, Icon, name }, idx) => {
+        const classes = [
+          'flex gap-4 rounded-[8px] items-center py-4 w-max transition-all duration-[.2s]',
+        ];
         return (
           <li key={idx}>
             <NavLink
@@ -82,8 +85,11 @@ function IconsBox() {
                   props.isActive &&
                   location.pathname === '/dashboard') ||
                 (props.isActive && idx > 0)
-                  ? 'transition-all duration-[.2s] py-4 px-8 bg-color-green-light text-black'
-                  : ''
+                  ? [
+                      ...classes,
+                      'transition-all duration-[.2s] py-4 px-8 bg-color-green-light text-black',
+                    ]
+                  : [...classes]
               }
             >
               <Icon fill={'white'} />
