c8621589cd9037fcfcd1d0a32f3ef1bac685a69e
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 18:55:26 +0100

    working on ref add sos

--- src/components/icons/IconsBox.tsx
+++ src/components/icons/IconsBox.tsx
@@ -77,8 +77,8 @@ function IconsBox() {
           <li key={idx}>
             <NavLink
               to={to}
-              className={(props) => idx === 0 && location.pathname === '/dashboard' ||
-                props.isActive 
+              className={(props) => idx === 0 && location.pathname === '/dashboard' 
+               
                   ? 'transition-all duration-[.2s] py-4 px-8 bg-color-green-light text-black'
                   : ''
               }
