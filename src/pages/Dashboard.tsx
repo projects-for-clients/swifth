3e146620473544da4b4a8b419790e26f9cd01810
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 13:14:22 +0100

    working on ref add sos

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router';
-import { useRef, useEffect, useState, MutableRefObject } from 'react';
 import Sidebar from '../components/dashboard/Sidebar';
-import { useDebouncedEvent } from '../components/utils/useDebouncedEvent';
+
 
 function Dashboard() {
   return (
