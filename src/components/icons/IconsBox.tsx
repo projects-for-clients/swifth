687b49d5562622f93f710f69a95f393390b90020
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 19:41:34 +0100

    working on ref add sos

--- src/components/icons/IconsBox.tsx
+++ src/components/icons/IconsBox.tsx
@@ -73,7 +73,7 @@ function IconsBox() {
   return (
     <ul className="grid gap-4 w-full">
       {LINKS.map(({ to, Icon, name }, idx) => {
-        const classes = `flex gap-4 rounded-[8px] items-center py-4 w-max transition-all duration-[.2s] hover:px-8 hover:bg-color-primary hover:text-black hover:translate-x-4 hover:scale-[1.01] [&>svg]:hover:fill-black `;
+        const classes = `flex gap-4 rounded-[8px] items-center py-4 w-max transition-all duration-[.2s] hover:px-8 hover:bg-color-primary hover:text-black hover:translate-x-4 hover:scale-[1.01] [&>svg]:hover:fill-black [&>svg]:hover:transition-colors [&>svg]:hover:duration-[.2s] [&>svg]:hover:delay-[.2s] `;
 
         return (
           <li key={idx}>
