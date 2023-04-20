e2b7f7b1694d24b32de64eb3a0870f62d632b4b5
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 20:01:48 +0100

    working on ref add sos

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -18,7 +18,7 @@ function Header({ title, subTitle, onboarding, openDialog }: THeader) {
     <div className="flex justify-between mb-[3rem]">
       <div className="flex items-center gap-4">
         <span className="bg-color-primary-dark flex p-2 rounded-full sm:hidden">
-          <BiMenu className="text-white w-[2.8rem] h-[2.8rem]" />
+          <BiMenu className="text-white w-[2rem] h-[2rem]" />
         </span>
         <div className="grid">
           <p className="font-medium text-[2.4rem]">{title}</p>
