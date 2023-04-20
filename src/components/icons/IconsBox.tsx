351fd1a58d92946aca68a8b8cbdbdf40a1c696fc
Igboanugwo Collins <munisco12@gmail.com>
2023-04-20 18:20:20 +0100

    working on ref add sos

--- src/components/icons/IconsBox.tsx
+++ src/components/icons/IconsBox.tsx
@@ -38,7 +38,7 @@ function IconsBox() {
                     : ''
                 }
               >
-                <Icon fill='white'/>
+                {<Icon/>}
                 <span>{name}</span>
               </NavLink>
             </li>
