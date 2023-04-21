75d7d2232ac10a1b3a1a79d8931decba0ef7c6ac
Igboanugwo Collins <munisco12@gmail.com>
2023-04-21 11:31:36 +0100

    working on responsiveness

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -45,7 +45,7 @@ function Sidebar() {
       ref={sidebarRef}
     >
       <div
-        className={`absolute p-16 min-w-[30rem] h-[80rem] flex flex-col justify-between text-white bg-color-primary-dark ${
+        className={`absolute p-16 min-w-[30rem] sm:min-w-0 h-[80rem] flex flex-col justify-between text-white bg-color-primary-dark ${
           openSidebar ? 'items-start' : 'items-center'
         }`}
       >
